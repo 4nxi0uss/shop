5950dcbd3f328978e832840da3295ea3741ffb97
Artur Zaczek <artur200212@interia.pl>
2022-08-18 15:42:10 +0200

    working on header

--- client/src/Components/Header/Header.tsx
+++ client/src/Components/Header/Header.tsx
@@ -1,11 +1,35 @@
 import * as React from 'react';
 
-import './Header.scss'
+import style from './Header.module.scss'
+
+import block from 'bem-css-modules'
+
+import Logo from '../../img/logo.svg'
+import Cart from '../../img/icon-cart.svg'
+import avatar from '../../img/image-avatar.png'
+
+const b = block(style)
 
 const Header = () => {
+
     return (
-        <header>
-            <h1>header</h1>
+        <header className={b()}>
+            <div className={b('div')}>
+                <img src={Logo} alt='logo' className={b('logo')} />
+
+                <ul className={b('menu')}>
+                    <li>Collections</li>
+                    <li>Men</li>
+                    <li>Women</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+            <div className={b('div')}>
+                <img src={Cart} alt='cart' className={b('cart')} />
+                <img src={avatar} alt='avatar' className={b('avatar')} />
+            </div>
+
         </header>
     )
 }
